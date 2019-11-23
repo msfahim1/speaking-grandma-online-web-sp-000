b182a5f0bdb117c8a0e5faba68e7f86d1b2e3846
Michael Fahim <michael_fahim@yahoo.com>
2019-11-23 19:58:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
 
   if phrase == phrase.uppercase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
